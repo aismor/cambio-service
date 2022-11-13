98cbaa85fa6174b8965eb51ca7c93be796aea265
Bruno Morais <brunodemoraiss@gmail.com>
2022-11-13 09:41:32 -0300

    go

---
+++ src/main/java/br/com/aismor/controller/CambioController.java
@@ -0,0 +1,46 @@
+package br.com.aismor.controller;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.aismor.model.Cambio;
+import br.com.aismor.repository.CambioRepository;
+
+@RestController()
+@RequestMapping("/cambio-service")
+public class CambioController {
+
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CambioRepository cambioRepository;
+	
+	@GetMapping(value ="/{amount}/{from}/{to}")
+	public Cambio getCambio(
+			@PathVariable("amount") BigDecimal amount,
+			@PathVariable("from") String from,
+			@PathVariable("to") String to) {
+		
+		var cambio = cambioRepository.findByFromAndTo(from, to);
+		
+		if(cambio == null) throw new RuntimeException("Currency Unsupported");
+		var port = environment.getProperty("local.server.port");
+
+		BigDecimal conversionFactior = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactior.multiply(amount);
+		
+		cambio.setConvertedValue(convertedValue.setScale(2,RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		
+		return cambio;
+	}
+
+}
